0dbf86ead28f49297819095cdd39735ed7c99032
sangeetagupta <tinnigupta1998@gmail.com>
2019-06-09 01:39:54 +0530

    created stateful widget ball wherein the central image is randomly changed on press.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[200],
+      child: Center(
+        child: FlatButton(
+          onPressed: () { _randomImageIndexGenerator(); },
+          child: Image.asset('images/ball${ballNumber +1}.png'),
+        ),
+      ),
+    );
+  }
+
+  void _randomImageIndexGenerator(){
+     Random random = new Random();
+
+     setState(() {
+       ballNumber = random.nextInt(4);
+     });
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
